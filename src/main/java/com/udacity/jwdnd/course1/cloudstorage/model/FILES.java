c7ca3c60be0d17a08a71343213bfae05c76f0b4f
alvahuiwork@gmail.com <alvahuiwork@gmail.com>
2021-01-03 22:22:34 +0800

    1-3

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/FILES.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/FILES.java
@@ -22,23 +22,47 @@ public class FILES {
         return fileId;
     }
 
+    public void setFileId(Integer fileId){
+        this.fileId=fileId;
+    }
+
     public String getFilename(){
         return filename;
+    }
+
+    public void setFilename(String filename){
+        this.filename = filename;
     }
 
     public String getContenttype(){
         return contenttype;
     }
 
+    public void setContenttype(String contenttype){
+        this.contenttype=contenttype;
+    }
+
     public String getFilesize(){
         return filesize;
+    }
+
+    public void setFilesize(String filesize){
+        this.filesize=filesize;
     }
 
     public Integer getUserid(){
         return userid;
     }
 
+    public void setUserid(Integer userid){
+        this.userid=userid;
+    }
+
     public BLOB getFiledata(){
         return filedata;
+    }
+
+    public void setFiledata(BLOB filedata){
+        this.filedata=filedata;
     }
 }
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/NOTES.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/NOTES.java
@@ -18,15 +18,31 @@ public class NOTES {
         return noteid;
     }
 
+    public void setNoteid(Integer noteid){
+        this.noteid = noteid;
+    }
+
     public String getNotetitle(){
         return notetitle;
+    }
+
+    public void setNotetitle(String notetitle){
+        this.notetitle = notetitle;
     }
 
     public String getNotedescription(){
         return notedescription;
     }
 
+    public void setNotedescription(String notedescription){
+        this.notedescription = notedescription;
+    }
+
     public Integer getUserid(){
         return userid;
+    }
+
+    public void setUserid(Integer userid){
+        this.userid=userid;
     }
 }
